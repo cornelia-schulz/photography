58e5497261e46a36833c7319e3ca135f4ee1c053
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-05-14 17:11:33 +1000

    set up i18n

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {getAllPhotos} from '../apiClient'
+import {withNamespaces} from 'react-i18next'
 
 class App extends React.Component {
   constructor(props){
@@ -30,4 +31,4 @@ class App extends React.Component {
   }
 
 }
-export default App
+export default withNamespaces('strings')(App)
--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import i18n from './i18n'
 
 import App from './components/App'
+import { I18nextProvider } from 'react-i18next';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDom.render(
-    <App />,
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>,
     document.getElementById('app')
   )
 })
