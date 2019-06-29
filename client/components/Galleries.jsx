2d3da69a57fd1a9ee4a1b2464f373a7cb82937fc
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-06-29 23:20:37 +1000

    start adding galleries

--- client/components/Galleries.jsx
+++ client/components/Galleries.jsx
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react'
 function Galleries() {
 
     return(
-        <div className="about">
+        <div className="galleries container">
             <h1>Galleries</h1>
+            <div className="gallery"></div>
         </div>
     )
 }
