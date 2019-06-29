9a1f721ea0386dc8b93dbda5b81be3ca00276f02
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-06-30 00:08:59 +1000

    add overlay to gallery

--- client/components/Galleries.jsx
+++ client/components/Galleries.jsx
@@ -5,7 +5,12 @@ function Galleries() {
     return(
         <div className="galleries container">
             <h1>Galleries</h1>
-            <div className="gallery"></div>
+            <div className="gallery-image-container">
+                <div className="gallery"></div>
+                <div className="gallery-overlay">
+                    <p className="gallery-overlay-text">Astro</p>
+                </div>
+            </div> 
         </div>
     )
 }
