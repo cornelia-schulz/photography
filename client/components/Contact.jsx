457a7268ea74f6e6bd389f9793ce62fac3980014
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-06-29 20:22:31 +1000

    add metatags and basic contact form

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -3,8 +3,17 @@ import React, { useState, useEffect } from 'react'
 function Contact() {
 
     return(
-        <div className="about">
-            <h1>Contact me</h1>
+        <div className="contact container">
+            <h1>Say hello</h1>
+            <p>If you have any questions or just want to say hello, drop me a line below!</p>
+            <form>
+                <label for="contactName">Your name:</label>
+                <input id="contactName" type="text" />
+                <label for="contactEmail">Your email:</label>
+                <input id="contactEmail" type="email" />
+                <label for="contactMessage" rows="5">Your message:</label>
+                <textarea></textarea>
+            </form>
         </div>
     )
 }
