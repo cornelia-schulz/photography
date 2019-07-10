f6091725c2833a0621307848e3e1524c82d99443
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-07-10 21:22:41 +1000

    more data

--- seeds/photo_details.js
+++ seeds/photo_details.js
@@ -5,12 +5,13 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('photo_details').insert([
-        {id: 1, photo_id: 1, language_id: 1, gallery_id: null, title: 'Habour Bridge in Sydney', description: '', location: 'Australia', category: 'cityscape'},
-        {id: 2, photo_id: 1, language_id: 2, gallery_id: null, title: 'Habour Bridge in Sydney', description: '', location: 'Australia', category: 'cityscape'},
-        {id: 3, photo_id: 2, language_id: 1, gallery_id: null, title: 'McLaren Falls', description: '', location: 'New Zealand', category: 'landscape'},
-        {id: 4, photo_id: 2, language_id: 2, gallery_id: null, title: 'McLaren Falls', description: '', location: 'New Zealand', category: 'landscape'},
-        {id: 5, photo_id: 3, language_id: 1, gallery_id: null, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'},
-        {id: 6, photo_id: 3, language_id: 2, gallery_id: null, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'}
+        {id: 1, photo_id: 1, language_id: 1, gallery_id: 3, title: 'Habour Bridge in Sydney', description: '', location: 'Australia', category: 'cityscape'},
+        {id: 2, photo_id: 1, language_id: 2, gallery_id: 3, title: 'Habour Bridge in Sydney', description: '', location: 'Australia', category: 'cityscape'},
+        {id: 3, photo_id: 2, language_id: 1, gallery_id: 1, title: 'McLaren Falls', description: '', location: 'New Zealand', category: 'landscape'},
+        {id: 4, photo_id: 2, language_id: 2, gallery_id: 1, title: 'McLaren Falls', description: '', location: 'New Zealand', category: 'landscape'},
+        {id: 5, photo_id: 3, language_id: 1, gallery_id: 1, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'},
+        {id: 6, photo_id: 3, language_id: 2, gallery_id: 1, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'},
+        {id: 7, photo_id: 4, language_id: 1, gallery_id: 3, title: 'Auckland New Years', description: '', location: 'New Zealand', category: 'cityscape'}
       ]);
     });
 };
--- seeds/photos.js
+++ seeds/photos.js
@@ -7,7 +7,10 @@ exports.seed = function(knex, Promise) {
       return knex('photos').insert([
         {id: 1, title: 'Sydney Harbour Bridge', link: '/images/harbour-bridge-sydney.jpg'},
         {id: 2, title: 'McLaren Falls', link: '/images/mclaren-falls.jpg'},
-        {id: 3, title: 'White Island', link: '/images/white-island.jpg'}
+        {id: 3, title: 'White Island', link: '/images/white-island.jpg'},
+        {id: 4, title: 'New Year fireworks', link: '/images/auckland-new-years.jpg'},
+        {id: 5, title: 'Harbour Bridge View', Link: '/images/harbour-bridge-sydney1.jpg'},
+        {id: 6, title: 'Harbour Bridge', Link: '/images/harbour-bridge-sydney2.jpg'}
       ]);
     });
 };
