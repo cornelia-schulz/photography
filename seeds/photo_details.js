209df0b89e7db735ecec688b7e3e8fb0340d07b8
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-08-27 21:15:28 +1000

    update seeds and images

--- seeds/photo_details.js
+++ seeds/photo_details.js
@@ -11,7 +11,11 @@ exports.seed = function(knex, Promise) {
         {id: 4, photo_id: 2, language_id: 2, gallery_id: 1, title: 'McLaren Falls', description: '', location: 'New Zealand', category: 'landscape'},
         {id: 5, photo_id: 3, language_id: 1, gallery_id: 1, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'},
         {id: 6, photo_id: 3, language_id: 2, gallery_id: 1, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape'},
-        {id: 7, photo_id: 4, language_id: 1, gallery_id: 3, title: 'Auckland New Years', description: '', location: 'New Zealand', category: 'cityscape'}
+        {id: 7, photo_id: 4, language_id: 1, gallery_id: 3, title: 'Auckland New Years', description: '', location: 'New Zealand', category: 'cityscape'},
+        {id: 8, photo_id: 5, language_id: 1, gallery_id: 3, title: 'Harbour Bridge View', description: '', location: 'Australia', category: 'cityscape'},
+        {id: 9, photo_id: 6, language_id: 1, gallery_id: 2, title: 'Harbour Bridge', description: '', location: 'Australia', category: 'cityscape'},
+        {id: 10, photo_id: 7, language_id: 1, gallery_id: 3, title: 'Darling Harbour', description: '', location: 'Australia', category: 'cityscape'},
+        {id: 11, photo_id: 8, language_id: 1, gallery_id: 2, title: 'Muriwai', description: '', location: 'New Zealand', category: 'astro'}
       ]);
     });
 };
--- seeds/photos.js
+++ seeds/photos.js
@@ -5,12 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       // Inserts seed entries
       return knex('photos').insert([
-        {id: 1, title: 'Sydney Harbour Bridge', link: '/images/harbour-bridge-sydney.jpg'},
-        {id: 2, title: 'McLaren Falls', link: '/images/mclaren-falls.jpg'},
-        {id: 3, title: 'White Island', link: '/images/white-island.jpg'},
-        {id: 4, title: 'New Year fireworks', link: '/images/auckland-new-years.jpg'},
+        {id: 1, title: 'Sydney Harbour Bridge', Link: '/images/harbour-bridge-sydney.jpg'},
+        {id: 2, title: 'McLaren Falls', Link: '/images/mclaren-falls.jpg'},
+        {id: 3, title: 'White Island', Link: '/images/white-island.jpg'},
+        {id: 4, title: 'New Year fireworks', Link: '/images/auckland-new-years.jpg'},
         {id: 5, title: 'Harbour Bridge View', Link: '/images/harbour-bridge-sydney1.jpg'},
-        {id: 6, title: 'Harbour Bridge', Link: '/images/harbour-bridge-sydney2.jpg'}
+        {id: 6, title: 'Harbour Bridge', Link: '/images/harbour-bridge-sydney2.jpg'},
+        {id: 7, title: 'Darling Harbour', Link: '/images/darling-harbour.jpg'},
+        {id: 8, title: 'Muriwai', Link: '/images/muriwai-astro.jpg'}
       ]);
     });
 };
