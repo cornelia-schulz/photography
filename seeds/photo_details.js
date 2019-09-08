7b34e425bd0fd83834d0c39f84f568d56599dfd4
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-09-08 22:29:47 +1000

    add more images

--- seeds/photo_details.js
+++ seeds/photo_details.js
@@ -13,9 +13,15 @@ exports.seed = function(knex, Promise) {
         {id: 6, photo_id: 3, language_id: 2, gallery_id: 1, title: 'White Island', description: '', location: 'New Zealand', category: 'landscape', favourite: 'no'},
         {id: 7, photo_id: 4, language_id: 1, gallery_id: 3, title: 'Auckland New Years', description: '', location: 'New Zealand', category: 'cityscape', favourite: 'no'},
         {id: 8, photo_id: 5, language_id: 1, gallery_id: 3, title: 'Harbour Bridge View', description: '', location: 'Australia', category: 'cityscape', favourite: 'no'},
-        {id: 9, photo_id: 6, language_id: 1, gallery_id: 2, title: 'Harbour Bridge', description: '', location: 'Australia', category: 'cityscape', favourite: 'no'},
+        {id: 9, photo_id: 6, language_id: 1, gallery_id: 3, title: 'Harbour Bridge', description: '', location: 'Australia', category: 'cityscape', favourite: 'no'},
         {id: 10, photo_id: 7, language_id: 1, gallery_id: 3, title: 'Darling Harbour', description: '', location: 'Australia', category: 'cityscape', favourite: 'yes'},
-        {id: 11, photo_id: 8, language_id: 1, gallery_id: 2, title: 'Muriwai', description: '', location: 'New Zealand', category: 'astro', favourite: 'yes'}
+        {id: 11, photo_id: 8, language_id: 1, gallery_id: 2, title: 'Muriwai', description: '', location: 'New Zealand', category: 'astro', favourite: 'yes'},
+        {id: 12, photo_id: 9, language_id: 1, gallery_id: 1, title: 'Anawhata', description: '', location: 'New Zealand', category: 'astro', favourite: 'no'},
+        {id: 13, photo_id: 10, language_id: 1, gallery_id: 2, title: 'Milkyway over Bethells', description: '', location: 'New Zealand', category: 'astro', favourite: 'yes'},
+        {id: 14, photo_id: 11, language_id: 1, gallery_id: 2, title: 'Bethells', description: '', location: 'New Zealand', category: 'astro', favourite: 'no'},
+        {id: 15, photo_id: 12, language_id: 1, gallery_id: 2, title: 'Pakiri', description: '', location: 'New Zealand', category: 'astro', favourite: 'no'},
+        {id: 16, photo_id: 13, language_id: 1, gallery_id: 2, title: 'Whatipu', description: '', location: 'New Zealand', category: 'astro', favourite: 'no'},
+        {id: 17, photo_id: 14, language_id: 1, gallery_id: 2, title: 'Milkyway over Whatipu', description: '', location: 'New Zealand', category: 'astro', favourite: 'no'}
       ]);
     });
 };
--- seeds/photos.js
+++ seeds/photos.js
@@ -12,7 +12,13 @@ exports.seed = function(knex, Promise) {
         {id: 5, title: 'Harbour Bridge View', Link: '/images/harbour-bridge-sydney1.jpg'},
         {id: 6, title: 'Harbour Bridge', Link: '/images/harbour-bridge-sydney2.jpg'},
         {id: 7, title: 'Darling Harbour', Link: '/images/darling-harbour.jpg'},
-        {id: 8, title: 'Muriwai', Link: '/images/muriwai-astro.jpg'}
+        {id: 8, title: 'Muriwai', Link: '/images/muriwai-astro.jpg'},
+        {id: 9, title: 'Anawhata', Link: '/images/anawhata.jpg'},
+        {id: 10, title: 'Milkyway over Bethells', Link: '/images/bethells-milkyway.jpg'},
+        {id: 11, title: 'Bethells', Link: '/images/bethells.jpg'},
+        {id: 12, title: 'Pakiri', Link: '/images/pakiri.jpg'},
+        {id: 13, title: 'Whatipu', Link: '/images/whatipu-river.jpg'},
+        {id: 14, title: 'Milkyway over Whatipu', Link: '/images/whatipu-milkyway.jpg'}
       ]);
     });
 };
